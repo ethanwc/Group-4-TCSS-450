b6d43f6552741a117ed3f2e4a6471172056f1464
StevenGhub <stevenhuang5132@gamil.com>
2019-01-26 18:03:15 -0800

    finsih fornt end skeleton

---
+++ Wessenger/app/src/main/java/ethanwc/tcss450/uw/edu/template/RequestsInvitationsFragment.java
@@ -0,0 +1,29 @@
+package ethanwc.tcss450.uw.edu.template;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RequestsInvitationsFragment extends Fragment {
+
+
+    public RequestsInvitationsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_requests_invitations, container, false);
+    }
+
+}
---
+++ Wessenger/app/src/main/java/ethanwc/tcss450/uw/edu/template/SaveLocationsFragment.java
@@ -0,0 +1,29 @@
+package ethanwc.tcss450.uw.edu.template;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SaveLocationsFragment extends Fragment {
+
+
+    public SaveLocationsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_save_locations, container, false);
+    }
+
+}
