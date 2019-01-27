b61b263cc6a81a9147373fe2d69b04797012faac
Arjunpjpt <arjunpjpt@gmail.com>
2019-01-27 00:34:20 -0800

    Navigate through all Fragments Edit the Drawer Menu, add navigation and add icons

--- Wessenger/app/src/main/java/ethanwc/tcss450/uw/edu/template/NewMessageFragment.java
+++ Wessenger/app/src/main/java/ethanwc/tcss450/uw/edu/template/NewMessageFragment.java
@@ -1,18 +1,22 @@
 package ethanwc.tcss450.uw.edu.template;
 
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NewMessageFragment extends Fragment {
-
+    private OnSendBtnNewMessage mListener;
 
     public NewMessageFragment() {
         // Required empty public constructor
@@ -23,7 +27,52 @@ public class NewMessageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_message, container, false);
+        View v = inflater.inflate(R.layout.fragment_new_message, container, false);
+
+        Button btnSend =(Button) v.findViewById(R.id.btn_newMessageFragment_send);
+        btnSend.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                mListener.onSendFragmentInteraction();
+            }
+
+
+        });
+        return v;
     }
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnSendBtnNewMessage) {
+            mListener = (OnSendBtnNewMessage) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     *
+     */
+    public interface OnSendBtnNewMessage {
+        // TODO: Update argument type and name
+        void onSendFragmentInteraction();
+    }
+
+
 
 }
