347a3b804ddd080b886763d5683707b1950cf51f
user <agcannon9@gmail.com>
2019-02-04 15:32:16 -0800

    ui changes

---
+++ Wessenger/app/src/main/java/ethanwc/tcss450/uw/edu/template/Messenger/InvitationViewFragment.java
@@ -0,0 +1,31 @@
+package ethanwc.tcss450.uw.edu.template.Messenger;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ethanwc.tcss450.uw.edu.template.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class InvitationViewFragment extends Fragment {
+
+
+    public InvitationViewFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_invitation_view, container, false);
+    }
+
+}
