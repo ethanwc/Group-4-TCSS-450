4b2e4a09b43f4257bf0da0e7e0fd3f791e416b2a
ethanwc <ethanwc@u.washington.edu>
2019-03-02 16:09:27 -0800

    added emojis...i guess

--- Wessenger/app/src/main/java/ethanwc/tcss450/uw/edu/template/Messenger/MessagingHomeActivity.java
+++ Wessenger/app/src/main/java/ethanwc/tcss450/uw/edu/template/Messenger/MessagingHomeActivity.java
@@ -12,11 +12,15 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.text.emoji.EmojiCompat;
+import android.support.text.emoji.FontRequestEmojiCompatConfig;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.provider.FontRequest;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -133,6 +137,7 @@ public class MessagingHomeActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         //init cloudinary stuffs
         MediaManager.init(this);
+        setupEmojis();
 
         setContentView(R.layout.activity_messaging_home);
         Toolbar toolbar = findViewById(R.id.toolbar_messenging_toolbar);
@@ -199,6 +204,37 @@ public class MessagingHomeActivity extends AppCompatActivity
         } else {
             loadChats();
         }
+    }
+
+    /**
+     * Initializes the download of emojis.
+     */
+    public void setupEmojis() {
+        final EmojiCompat.Config config;
+
+            // Use a downloadable font for EmojiCompat
+            final FontRequest fontRequest = new FontRequest(
+                    "com.google.android.gms.fonts",
+                    "com.google.android.gms",
+                    "Noto Color Emoji Compat",
+                    R.array.com_google_android_gms_fonts_certs);
+            config = new FontRequestEmojiCompatConfig(getApplicationContext(), fontRequest);
+
+
+        config.setReplaceAll(true)
+                .registerInitCallback(new EmojiCompat.InitCallback() {
+                    @Override
+                    public void onInitialized() {
+                        Log.i("EMOJISTUFF", "EmojiCompat initialized");
+                    }
+
+                    @Override
+                    public void onFailed(@Nullable Throwable throwable) {
+                        Log.e("EMOJISTUFF", "EmojiCompat initialization failed", throwable);
+                    }
+                });
+
+        EmojiCompat.init(config);
     }
 
     /**
